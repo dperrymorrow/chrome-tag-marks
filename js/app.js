648e3e9d2a231af2f78e4e073e28a984cdc8a33e
David Morrow <dperrymorrow@gmail.com>
2013-05-21 10:06:36 -0700

    getting started with ember

--- js/app.js
+++ js/app.js
@@ -1,24 +1,49 @@
-chrome.bookmarks.search('javascript', function (result){
-  console.dir(result);
-  $("content").html(result.toString());
-});
+// chrome.bookmarks.search('javascript', function (result){
+//   console.dir(result);
+//   $("content").html(result.toString());
+// });
 
 
 App = Ember.Application.create();
 
+
 App.Router.map(function() {
-  // put your routes here
+  return this.route('bookmarks', {
+    path: '/'
+  });
 });
 
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    // return ['red', 'yellow', 'blue'];
-
-    // debugger
-
-    chrome.bookmarks.search('javascript', function (result){
-      console.dir(result);
-      return result;
-    });
+App.BookmarksRoute = Ember.Route.extend({
+  setupController: function(controller) {
+    return controller.set('content', []);
   }
 });
+
+
+App.BookmarksController = Ember.Controller.extend({
+  bookmarks: [],
+
+  addBookmark: function() {
+    this.bookmarks.pushObject(Ember.Object.create({
+      name: this.get('newName')
+    }));
+    return this.set('newName', "");
+  }
+});
+
+
+
+// App.IndexRoute = Ember.Route.extend({
+//   model: function() {
+//     return ['red', 'yellow', 'blue'];
+
+//     // debugger
+
+//     // chrome.bookmarks.search('javascript', function (result){
+//     //   console.dir(result);
+//     //   return result;
+//     // });
+//   }
+// });
+
+
